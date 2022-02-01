d1a75cf9cb436a00fd81e26dd981149414d13997
Statcomm <63654894+Statcomm@users.noreply.github.com>
2022-02-01 14:20:36 +0300

    Forgot what we're honestly

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -1,59 +1,59 @@
 const Product = require("../../db/models/Product")
 let products = require("../../data")
 
-exports.getProduct = async (req,res)=>{
+exports.getProduct = async (req,res, next)=>{
    try {
       const products = await Product.find()
        res.json(products)
    } catch (error){
-       res.status(500).json({message:error.message})
+    next(error)
    }
 }
 
-exports.fetchProduct = async (req,res)=>{
+exports.fetchProduct = async (req,res, next)=>{
     try {
        const products = await Product.find({}).select("name description price color")
         res.json(products)
     } catch (error){
-        res.status(500).json({message:error.message})
+        next(error)
     }
  }
  
 
-exports.getDetail = (req,res)=>{
+exports.getDetail = (req,res, next)=>{
     const {productsId} = req.params
     const productsOne= products.find((e)=>e.id===+productsId)
     res.status(200).json(productsOne)
 }
 
-exports.createProduct = async (req,res) => {
+exports.createProduct = async (req,res, next) => {
   try { 
   const newProduct = await Product.create(req.body)
         res.status(201).json(newProduct)
   }  catch (error) {
-      res.status(500).json({message:error.message})
+    next(error)
   }
 }
 
 
 
 
-exports.deleteProduct = async (req,res) => {
+exports.deleteProduct = async (req,res, next) => {
     try {
         const {productId} = req.params
         const foundProduct = await Product.findByIdAndDelete ({_id: productId})
         if (foundProduct){
             res.status(204).end()
         } else {
-    res.status(404).json({message:"not found"})
+            next({status : 404, message: "Product not found"})
         }
 
     } catch (error) {
-        res.status(500).json({message:error.message})
+        next(error)
     }
 }
 
-exports.updateProduct = async (req, res) => {
+exports.updateProduct = async (req, res, next) => {
     try{
         const {productId} = req.params
         const product = await Product.findByIdAndUpdate({_id: productId}, req.body, {
@@ -61,8 +61,8 @@ exports.updateProduct = async (req, res) => {
             runValidators:true
         })
         if (product) res.status(200).json(product)
-        else res.status(404).json({message:"not found"})
+        else next({status : 404, message: "Product not found"})
     } catch (error) {
-        res.status(500).json({message: error.message})
+        next(error)
     }
 }
--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const productRoutes = require("./apis/products/routes")
 const connectDb = require("./db/database");
 
 app.use(express.json())
+
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get("host")}${req.path} `)
+    next()
+}
+)
+
 app.use("/api/products", productRoutes)
+
+app.use((req,res)=> {
+    res.status(404).json({message:"Path Not Found"})
+}
+)
+app.use((error, req,res, next)=> {
+    res.status(error.status || 500).json({message:error.message || "Something's not right..."})
+}
+)
 
 connectDb();
 app.listen(8000)
